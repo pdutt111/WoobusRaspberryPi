edd974aa6d79c780ea8b3a92d97648d3656b62fd
pariskshitdutt <pdutt111>
2016-01-04 03:46:27 +0530

    gpsd enabled

--- jobs/gpsd.js
+++ jobs/gpsd.js
@@ -1,58 +1,58 @@
-///**
-// * Created by pariskshitdutt on 25/10/15.
-// */
-//var events=require('../events');
-//var config= require('config');
-//var log = require('tracer').colorConsole(config.get('log'));
-//try{
-//    var gpsd = require('node-gpsd');
-//
-//    var daemon = new gpsd.Daemon({
-//        program: 'gpsd',
-//        device: '/dev/ttyAMA0',
-//        port: 2947,
-//        pid: '/tmp/gpsd.pid',
-//        logger: {
-//            info: function(info) {
-//                log.info(info);
-//            },
-//            warn: function(warn){
-//                log.warn(warn);
-//            },
-//            error: function(warn){
-//                log.warn(warn);
-//            }
-//        }
-//    });
-//    var listener = new gpsd.Listener({
-//        port: 2947,
-//        hostname: 'localhost',
-//        logger:  {
-//            info: function(info) {
-//                log.info(info)
-//            },
-//            warn: function(warn){
-//                log.warn(warn);
-//            },
-//            error: function(warn){
-//                log.warn(warn);
-//            }
-//        },
-//        parse: true
-//    });
-//    daemon.start(function() {
-//        console.log('Started');
-//        listener.connect(function() {
-//            console.log('Connected');
-//        });
-//        listener.watch();
-//        listener.on('TPV',function(tpvData){
-//            events.emitter.emit("location",tpvData);
-//        });
-//    });
-//}catch(e){
-//    log.info(e);
-//}
-//
-//
-//
+/**
+ * Created by pariskshitdutt on 25/10/15.
+ */
+var events=require('../events');
+var config= require('config');
+var log = require('tracer').colorConsole(config.get('log'));
+try{
+    var gpsd = require('node-gpsd');
+
+    var daemon = new gpsd.Daemon({
+        program: 'gpsd',
+        device: '/dev/ttyAMA0',
+        port: 2947,
+        pid: '/tmp/gpsd.pid',
+        logger: {
+            info: function(info) {
+                log.info(info);
+            },
+            warn: function(warn){
+                log.warn(warn);
+            },
+            error: function(warn){
+                log.warn(warn);
+            }
+        }
+    });
+    var listener = new gpsd.Listener({
+        port: 2947,
+        hostname: 'localhost',
+        logger:  {
+            info: function(info) {
+                log.info(info)
+            },
+            warn: function(warn){
+                log.warn(warn);
+            },
+            error: function(warn){
+                log.warn(warn);
+            }
+        },
+        parse: true
+    });
+    daemon.start(function() {
+        console.log('Started');
+        listener.connect(function() {
+            console.log('Connected');
+        });
+        listener.watch();
+        listener.on('TPV',function(tpvData){
+            events.emitter.emit("location",tpvData);
+        });
+    });
+}catch(e){
+    log.info(e);
+}
+
+
+
